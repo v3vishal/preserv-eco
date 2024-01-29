65e3172e0355afd53b4c0fb4c9f246bfedf18cd4
Vishal V V <vvvvishal2043@gmail.com>
2024-01-29 18:40:44 +0530

    Organised files and shifted testing folder to main

---
+++ jscode/staticforestcov.js
@@ -0,0 +1,254 @@
+/*// JS 
+var seriesPalette = ['#c9c9c9', '#9E9E9E']; 
+var differencePalette = ['#C8E6C9', '#FFAB91']; 
+  
+var datae = [ 
+  { 
+    category: 'Bandipur Forest', 
+    value2019: 49.67,
+    value2020: 53.13,
+  },
+]; 
+  
+function colorText(text, color) { 
+  return ( 
+    '<span color=' + 
+    color + 
+    '><b>' + 
+    text + 
+    '</b></span>'
+  ); 
+} 
+  
+var seriess = makeSeries(datae); 
+seriess.push( 
+  makeDifferenceSeries(seriess, differencePalette) 
+); 
+var e = seriess[2].points
+e[0].attributes_percent = (datae[0].value2020)-(datae[0].value2019)
+  
+var charte = JSC.chart('chartDiv2', { 
+  type: 'horizontal column solid', 
+  palette: seriesPalette, 
+  title_label_text: 
+    'Forest cover statistics in Bandipur Tiger Reserve, ' + 
+    colorText('2011', seriesPalette[0]) + 
+    ' and ' + 
+    colorText('2019', seriesPalette[1]) + 
+    ' comparison (in percentage %)', 
+  legend_visible: false, 
+  yAxis: { 
+    scale_range_padding: 0.15, 
+    formatString: 'n2'
+  }, 
+  defaultPoint: { 
+    tooltip: '%icon %seriesName: <b>%value</b>', 
+    radius: 0, 
+    outline_width: 0 
+  }, 
+  series: seriess 
+}); 
+  
+function makeSeries(datae) { 
+  var myNest = JSC.nest().key('category'); 
+  return [ { 
+      name: '2011', 
+      points: myNest 
+        .rollup('value2020') 
+        .points(datae) 
+    } ,
+    { 
+      name: '2019', 
+      points: myNest 
+        .rollup('value2019') 
+        .points(datae) 
+    }, 
+    
+  ]; 
+} 
+  */
+/** 
+ * Dynamically creates a difference series for the given array of two series. 
+ * @param seriess - array of two series 
+ * @param diffColors - array of two colors used for points depending on whether they are positive or negative. 
+ */
+/*
+function makeDifferenceSeries( 
+  seriess, 
+  diffColors 
+) { 
+  var ser1 = seriess[1], 
+    ser2 = seriess[0]; 
+  return { 
+    name: ser1.name + ' vs. ' + ser2.name, 
+    defaultPoint: { 
+      label: { 
+        text: '{%percent:n2}%', 
+        align: 'right'
+      }, 
+      tooltip: 
+        '%icon %seriesName: <b>{%yvalue-%ystart}</b>'
+    }, 
+    points: makeDifferencePoints( 
+      ser1.points, 
+      ser2.points 
+    ) 
+  }; 
+  
+  function makeDifferencePoints( 
+    points1, 
+    points2 
+  ) { 
+    return points1.map(function(p1, i) { 
+      var p2 = points2[i], 
+        y1 = p1.y, 
+        y2 = p2.y; 
+      return { 
+        x: p1.x, 
+        y: [y1, y2], 
+        color: 
+          y1 < y2 ? diffColors[0] : diffColors[1], 
+        attributes_percent: 100 - (y1 / y2) * 100 
+      }; 
+    }); 
+  } 
+} */
+// JS 
+var chart, data; 
+var palette = ['#4A7C59', '#8FC0A9']; 
+  
+/* Resembles underline Used to highlight selected label. */
+var selectedFill = { 
+  angle: 90, 
+  stops: [ 
+    [0, '#ffffff'], 
+    [0.94, '#ffffff'], 
+    [0.94, '#000000'], 
+    [1, '#000000'] 
+  ] 
+}; 
+
+JSC.fetch( 
+  '../res/stcforcov.csv'
+) 
+  .then(function(response) { 
+    return response.text(); 
+  }) 
+  .then(function(text) { 
+    data = JSC.csv2Json(text); 
+    chart = renderChart(makeSeries(data, 2020)); 
+  }) 
+  .catch(function(error) { 
+    console.error(error); 
+  });
+  
+function renderChart(series) { 
+  return JSC.chart('chartDiv3', { 
+    title: { 
+      label: { 
+        text: 
+          'Forest cover statistics in Bandipur Tiger Reserve (in percentage %)', 
+        margin: [0, 0, 25, 50] 
+      } 
+    }, 
+    animation_duration: 500, 
+    palette: palette, 
+    defaultSeries: { 
+      legendEntry_events_click: function() { 
+        return false; 
+      }, 
+      mouseTracking_enabled: true
+    }, 
+    defaultPoint: { 
+      outline_width: 0, 
+      label: { 
+        text: '%yValue%', 
+        placement: 'outside', 
+        autoHide: false
+      } 
+    }, 
+    type: 'horizontal column solid', 
+    legend: { 
+      position: 'bottom', 
+      template: '%icon %name', 
+      defaultEntry_cursor: 'default'
+    }, 
+    xAxis: { 
+      staticColumnWidth: 20, 
+      // Use the axis ticks as labels for each column stack 
+      defaultTick: { 
+        gridLine_visible: false, 
+        placement: 'inside', 
+        label_offset: '50,22'
+      } 
+    }, 
+    yAxis: { 
+      visible: false, 
+      scale: { 
+        type: 'stackedFull', 
+        /* Pad the axis range so that outside point labels fit inside the chart */
+        range_padding: 0.1, 
+        invert: true
+      } 
+    }, 
+    series: series, 
+    toolbar_items: { 
+      beforeAfterButtons: { 
+        label_text: '', 
+        position: 'inside top left', 
+        offset: '50,-25', 
+        itemsBox: { 
+          layout: 'horizontal', 
+          visible: true
+        }, 
+        defaultItem: { 
+          type: 'radio', 
+          padding: [0, 0, 4, -20], 
+          margin: 3, 
+          label_style: { 
+            fontSize: 14, 
+            color: '#9E9E9E'
+          }, 
+          icon_visible: false, 
+          states: { 
+            select: { 
+              fill: selectedFill, 
+              label_style: { color: '#000000' } 
+            }, 
+            hover_fill: selectedFill 
+          } 
+        }, 
+        events: { change: changeEvent }, 
+        value: '2020', 
+        items: { 
+          '2020': { label_text: '2020' }, 
+          '2060': { label_text: '2060' } 
+        } 
+      } 
+    } 
+  }); 
+} 
+  
+function changeEvent(val) { 
+  chart.options({ 
+    series: makeSeries(data, val) 
+  }); 
+} 
+  
+function makeSeries(data, year) { 
+  return JSC.nest() 
+    .key('age') 
+    .key('sex') 
+    .pointRollup(function(key, val) { 
+      var value = val[0], 
+        over65 = 
+          value.age === '65 years and over'; 
+      return { 
+        x: key, 
+        id: key + ' ' + value.age, 
+        y: value['year_' + year], 
+        label_align: over65 ? 'left' : 'right'
+      }; 
+    }) 
+    .series(data); 
+} 
---
+++ jscode/treetiger.js
@@ -0,0 +1,226 @@
+var trophicLvls = 4;
+        var data = [
+            { type: 'Tigers', value: 150 },
+        ];
+        var lastTLval = data[0].value
+        var lastmin1Val = lastTLval/0.1
+        var lastmin2Val = lastmin1Val/0.1
+        var lastmin3Val = lastmin2Val/0.1
+        lastmin3Val *= 5
+        data.push(
+            {type: 'Carnivores', value: Math.round(lastmin1Val/1.25)},
+            {type: 'Herbivores', value: Math.round(lastmin2Val/1.2)},
+            {type: 'Trees/Plants', value: Math.round(lastmin3Val)},);
+        var amount = 150;
+        var chart;
+        
+        
+
+        function initializeChart() {
+            var color = '#999900';
+            chart = JSC.chart('chartDiv', {
+                debug: true,
+                type: 'horizontal marker',
+                title_label: {
+                    text: 'Prediction for certain amount of tree/tiger existence based on current tiger/tree survival amount (<icon name=material/image/nature size=20 color=' + color + '> - ' + amount + ' Organisms)',
+                    style_fontSize: 16
+                },
+                palette: [color],
+                defaultSeries: {
+                    legendEntry_visible: false,
+                    mouseTracking_enabled: false,
+                    lastPoint_label: {
+                        text: '<b>%val</b>',
+                        align: 'right',
+                        style_fontSize: 14
+                    }
+                },
+                xAxis_defaultTick: {
+                    gridLine_visible: false,
+                    line_visible: false
+                },
+                yAxis_visible: false,
+                defaultPoint: {
+                    marker: {
+                        type: 'material/image/nature',
+                        size: 24,
+                        outline_width: 0
+                    },
+                    focusGlow: false
+                },
+                series: makeSeries(data)
+            });
+        }
+
+        function makeSeries(data) {
+            return data.map(function (item) {
+                var points = [],
+                    maxImages = Math.round(item.value / amount);
+                for (var i = 0; i < maxImages; i++) {
+                    points.push({
+                        x: item.type,
+                        y: i + 1,
+                        attributes_val: item.value
+                    });
+                }
+                return { name: item.name, points: points };
+            });
+        }
+
+
+        function updateTigerValue() {
+            var newTigerValue = parseFloat(document.getElementById('newTigerValue').value);
+            newTigerValue = Math.round(newTigerValue);
+            if (!isNaN(newTigerValue) && newTigerValue > 0) {
+                data = [ 
+                {type: "Tigers", value: newTigerValue}
+                ];
+                amount = newTigerValue;
+                var lastTLval = data[0].value
+                var lastmin1Val = Math.round(lastTLval/0.1)
+                var lastmin2Val = Math.round(lastmin1Val/0.1)
+                var lastmin3Val = Math.round(lastmin2Val/0.1)
+                lastmin3Val *= 5
+                Math.round(lastmin3Val);
+                data.push(
+                    {type: 'Carnivores', value: Math.round(lastmin1Val/1.25)},
+                    {type: 'Herbivores', value: Math.round(lastmin2Val/1.2)},
+                    {type: 'Trees/Plants', value: Math.round(lastmin3Val)},);
+                chart.destroy();
+                initializeChart();
+            } else {
+                alert('Please enter a valid numeric value for Tigers.');
+            }
+        }
+        function updateTreeValue() {
+            var newTreeValue = parseFloat(document.getElementById('newTreeValue').value);
+            newTreeValue = Math.round(newTreeValue);
+            if (!isNaN(newTreeValue) && newTreeValue > 0) {
+                data = [ 
+                {type: "Trees/Plants", value: newTreeValue}
+                ];
+                
+                var firstTLval = data[0].value
+                var firstpl1Val = Math.round(firstTLval*0.1)
+                var firstpl2Val = Math.round(firstpl1Val*0.1)
+                var firstpl3Val = Math.round(firstpl2Val*0.1)
+                firstpl3Val = Math.round(firstpl3Val/5)
+                if(firstpl3Val > 0) {
+                amount = firstpl3Val;
+                data.push(
+                    {type: 'Herbivores', value: Math.round(firstpl1Val/1.2)},
+                    {type: 'Carnivores', value: Math.round(firstpl2Val/1.25)},
+                    {type: 'Tigers', value: Math.round(firstpl3Val)},);
+                chart.destroy();
+                initializeChart();
+                } else {
+                    alert("These many trees won't suffice for one tiger. Food chain will end before reaching tiger. Error code: CHART_SCALE_0x0")
+                }
+            } else {
+                alert('Please enter a valid numeric value for Trees.');
+            }
+        }
+        function resetToCrntTgr() {
+            data = [ 
+                {type: "Tigers", value: 150}
+                ];
+                amount = 150;
+                var lastTLval = data[0].value
+                var lastmin1Val = lastTLval/0.1
+                var lastmin2Val = lastmin1Val/0.1
+                var lastmin3Val = lastmin2Val/0.1
+                lastmin3Val *= 5
+                data.push(
+                    {type: 'Carnivores', value: Math.round(lastmin1Val/1.25)},
+            {type: 'Herbivores', value: Math.round(lastmin2Val/1.2)},
+            {type: 'Trees/Plants', value: Math.round(lastmin3Val)},);
+                chart.destroy();
+                initializeChart();
+        }
+        function resetToCrntTree() {
+            data = [ 
+                {type: "Trees/Plants", value: 1350000}
+                ];
+                
+                var firstTLval = data[0].value
+                var firstpl1Val = firstTLval*0.1
+                var firstpl2Val = firstpl1Val*0.1
+                var firstpl3Val = firstpl2Val*0.1
+                firstpl3Val = firstpl3Val/5
+                amount = firstpl3Val;
+                data.push(
+                    {type: 'Herbivores', value: Math.round(firstpl1Val/1.2)},
+                    {type: 'Carnivores', value: Math.round(firstpl2Val/1.25)},
+                    {type: 'Tigers', value: Math.round(firstpl3Val)},);
+                chart.destroy();
+                initializeChart();
+        }
+        function updateHerbivoresValue() {
+            var newHerbivoresValue = parseFloat(document.getElementById('newHerbivoresValue').value);
+            newHerbivoresValue = Math.round(newHerbivoresValue);
+            if (!isNaN(newHerbivoresValue) && newHerbivoresValue > 0) {
+                    data = [ 
+                        {type: "Trees/Plants", value: 0},
+                        {type: "Herbivores", value: newHerbivoresValue}
+                    ];
+                    
+                    treeval = Math.round(newHerbivoresValue/0.1);
+                    herbval = Math.round(newHerbivoresValue);
+                    carnval = Math.round(newHerbivoresValue*0.1);
+                    tigrval = Math.round(carnval*0.1);
+                    tigrval = Math.round(tigrval/5);
+                    if(tigrval > 0){
+                    amount = tigrval;
+                    data = [];
+                    data.push(
+                        {type: "Trees/Plants", value: treeval*1.2},
+                        {type: "Herbivores", value: Math.round(herbval)},
+                        {type: "Carnivores", value: Math.round(carnval/1.25)},
+                        {type: "Tigers", value: Math.round(tigrval)}
+                    );
+                    chart.destroy();
+                    initializeChart();
+                    } else {
+                        alert("These many herbivores won't suffice for one tiger. Food chain will end before reaching tiger. Error code: CHART_SCALE_0x0");
+                    }
+            } else {
+                alert('Please enter a valid numeric value for Herbivores.');
+            }
+        }
+
+        function updateCarnivoresValue() {
+            var newCarnivoresValue = parseFloat(document.getElementById('newCarnivoresValue').value);
+            newCarnivoresValue = Math.round(newCarnivoresValue);
+            if (!isNaN(newCarnivoresValue) && newCarnivoresValue > 0) {
+                data = [ 
+                    {type: "Trees/Plants", value: 0},
+                    {type: "Herbivores", value: 0},
+                    {type: "Carnivores", value: newCarnivoresValue}
+                ];
+                
+                treeval = Math.round(herbval/0.1);
+                herbval = Math.round(newCarnivoresValue/0.1);
+                carnval = Math.round(newCarnivoresValue);
+                tigrval = Math.round(carnval*0.1);
+                tigrval = Math.round(tigrval/5);
+                if(tigrval > 0) {
+                amount = tigrval;
+                data = [];
+                data.push(
+                    {type: "Trees/Plants", value: treeval},
+                    {type: "Herbivores", value: Math.round(herbval/1.2)},
+                    {type: "Carnivores", value: Math.round(carnval/1.25)},
+                    {type: "Tigers", value: Math.round(tigrval)}
+                );
+                chart.destroy();
+                initializeChart();
+                } else {
+                    alert("These many carnivores won't suffice for one tiger. Food chain will end before reaching tiger. Error code: CHART_SCALE_0x0");
+                }
+            } else {
+                alert('Please enter a valid numeric value for Carnivores.');
+            }
+        }
+window.onload = function() {
+            initializeChart();
+        }
